833a5fde8b8789706a204ae1a12126a0ac2c08ce
Daniel Corcoran <danielc.92@outlook.com>
2019-07-06 08:46:39 +1000

    Added connection to socket

--- public/js/chat.js
+++ public/js/chat.js
@@ -2,3 +2,12 @@ console.log('If you are seeing this message, the chat.js has been included succe
 
 var current_id = document.getElementById('current-id');
 var current_username = document.getElementById('current-username');
+
+const url = 'http://localhost:3001';
+var socket_id;
+var socket = io(url);
+socket.on('connect', ()=> {
+    socket_id = socket.id;
+    current_id.innerText = socket_id;
+})
+console.log(socket_id)
