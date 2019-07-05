4a9a5d2bd0e7c6644647790804b6f9ec09dd154e
Daniel Corcoran <danielc.92@outlook.com>
2019-07-06 08:46:28 +1000

    Added user vars

---
+++ public/js/chat.js
@@ -0,0 +1,4 @@
+console.log('If you are seeing this message, the chat.js has been included successfully.')
+
+var current_id = document.getElementById('current-id');
+var current_username = document.getElementById('current-username');
